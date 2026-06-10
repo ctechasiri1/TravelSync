2d7d20224a429d5a81831e3bd2410fc31d5f81fd
Chiraphat Techasiri <ctechasiri@gmail.com>
2026-06-09 22:48:35 -0700

    Added pixel perfect launch screen and swiftlint to the project

--- TravelSync/Core/Login/Views/ContentView.swift
+++ TravelSync/Core/Login/Views/ContentView.swift
@@ -11,19 +11,20 @@ struct ContentView: View {
     @Environment(AppState.self) private var appState
     
     var body: some View {
-        Group {
-            switch appState.currentAuthScreen {
-            case .loading:
-                LoadingScreen()
-            case .signUp:
-                SignUpScreen(viewModel: appState.makeSignUpViewModel())
-            case .login:
-                LoginScreen(viewModel: appState.makeLoginViewModel())
-            case .home:
-                HomeScreen()
-            }
-        }
-        .animation(.easeInOut(duration: 0.3), value: appState.currentAuthScreen)
+        LoadingScreen()
+//        Group {
+//            switch appState.currentAuthScreen {
+//            case .loading:
+//                LoadingScreen()
+//            case .signUp:
+//                SignUpScreen(viewModel: appState.makeSignUpViewModel())
+//            case .login:
+//                LoginScreen(viewModel: appState.makeLoginViewModel())
+//            case .home:
+//                HomeScreen()
+//            }
+//        }
+//        .animation(.easeInOut(duration: 0.3), value: appState.currentAuthScreen)
     }
 }
 
