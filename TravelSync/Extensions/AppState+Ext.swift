8211ae8252e32c2cd70f1073cb2ce2ab62729b59
Chiraphat Techasiri <ctechasiri@gmail.com>
2026-09-19 01:25:30 -0700

    Removed some used extensions and renamed them

--- TravelSync/Extensions/AppState+Ext.swift
+++ TravelSync/Extensions/AppState+Ext.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-extension TSAppState {    
-    var deleteConfirmationManager: DeleteConfirmationManager {
-        managers.deleteConfirmationManager
-    }
-}
+//extension TSAppState {    
+//    var deleteConfirmationManager: DeleteConfirmationManager {
+//        managers.deleteConfirmationManager
+//    }
+//}
